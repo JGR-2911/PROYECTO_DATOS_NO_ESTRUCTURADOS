5b766c8cb520a321de37504dde4a6dc337cca7d3
AlejandraPG08 <102006570+AlejandraPG08@users.noreply.github.com>
2023-05-16 18:14:49 -0600

    index

--- admin/index.php
+++ admin/index.php
@@ -7,6 +7,10 @@ include("views/menu.php");
         <i class="fa-solid fa-bars"></i>
         <span class="text">Indicador de Sequías</span>
     </div>
+    <div class="contenedor">
+    <h1><b>Bienvenido</b></h1>
+    <p>Con este sistema puedes consultar ya se mediante el ingreso de datos o un archivo la existencia de una sequia.</p>
+    </div>
 </div>
 <?php
 include("views/footer.php");
